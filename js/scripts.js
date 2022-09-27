1ebdf5b8d179544d7244247c6a5804ea90e7c27b
Esau <esaurenteria.isc@gmail.com>
2022-09-27 10:18:18 -0500

    Agregando controles para cambiar el control primario

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,10 @@ const toggleTheme = document.getElementById('toggle-theme');
 const toggleIcon = document.getElementById('toggle-icon');
 const toggleText = document.getElementById('toggle-text');
 
+const toggleColors = document.getElementById("toggle-colors");
+
+const rootStyles = document.documentElement.style;
+
 toggleTheme.addEventListener('click', ()=>{
     document.body.classList.toggle('dark')
     if(toggleIcon.src.includes('moon.svg')){
@@ -13,3 +17,7 @@ toggleTheme.addEventListener('click', ()=>{
     }
 });
 
+
+toggleColors.addEventListener("click", (e) =>{
+    rootStyles.setProperty("--primary-color", e.target.dataset.color);
+});
